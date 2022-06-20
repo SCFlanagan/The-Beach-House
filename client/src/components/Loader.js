d1aa225a45cf2a61adc06e4dc7fd4ccbdd689e53
Susan Flanagan <susanflanagan@Susans-MacBook-Pro.local>
2022-06-20 03:15:45 -0400

    fixed loader positioning

--- client/src/components/Loader.js
+++ client/src/components/Loader.js
@@ -3,9 +3,11 @@ import { Spinner } from "react-bootstrap";
 
 function Loader() {
   return (
-    <Spinner animation="border" role="status" id="loading-spinner">
-      <span className="sr-only">Loading...</span>
-    </Spinner>
+    <div className="spinner-container">
+      <Spinner animation="border" role="status" id="loading-spinner">
+        <span className="sr-only">Loading...</span>
+      </Spinner>
+    </div>
   );
 }
 
